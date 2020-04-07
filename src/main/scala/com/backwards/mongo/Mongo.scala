2a720b6081a60fa9f9a8e8427b4564538b5948e4
David Ainslie <dainslie@gmail.com>
2020-04-07 21:05:40 +0100

    Refactor

--- src/main/scala/com/backwards/mongo/Mongo.scala
+++ src/main/scala/com/backwards/mongo/Mongo.scala
@@ -1,9 +1,28 @@
 package com.backwards.mongo
 
 import cats.effect.IO
+import cats.implicits._
+import fs2._
 import com.mongodb.reactivestreams.client.{MongoClient, MongoClients}
 
 object Mongo {
-  def mongoClient(config: IO[MongoConfig]): IO[MongoClient] =
-    IO(MongoClients.create("mongodb://localhost"))
+  def mongoClient(config: IO[MongoConfig]): Stream[IO, MongoClient] = {
+    val acquire = IO(MongoClients.create("mongodb://localhost"))
+
+    val release: MongoClient => IO[Unit] =
+      mongoClient => IO {
+        println("-- release baby ---")
+        mongoClient.close() }
+
+    Stream.bracket(acquire)(release)
+  }
 }
+
+/* TODO
+
+val settings: MongoClientSettings = MongoClientSettings
+  .builder()
+  .applyToClusterSettings(b => b.hosts(List(new ServerAddress("localhost")).asJava))
+  .build()
+
+*/
