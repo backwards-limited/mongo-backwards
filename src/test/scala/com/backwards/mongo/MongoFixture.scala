cea6c1455fb23271b6230ef7cb705ddcb45793d7
David Ainslie <dainslie@gmail.com>
2020-04-08 18:42:03 +0100

    Refactor

---
+++ src/test/scala/com/backwards/mongo/MongoFixture.scala
@@ -0,0 +1,28 @@
+package com.backwards.mongo
+
+import java.util.UUID
+import cats.effect.IO
+import cats.implicits._
+import fs2.Stream
+import org.mongodb.scala.bson.BsonDocument
+import org.mongodb.scala.result.InsertOneResult
+import com.mongodb.reactivestreams.client.{MongoClient, MongoCollection, MongoDatabase}
+import com.backwards.cassandra.User
+import com.backwards.mongo.bson.Encoder.ops._
+
+object MongoFixture extends MongoFixture
+
+trait MongoFixture {
+  def seed(mongoClient: Stream[IO, MongoClient]): Stream[IO, MongoClient] =
+    mongoClient evalTap { mongoClient =>
+      IO {
+        val database: MongoDatabase = mongoClient.getDatabase("mydatabase")
+
+        val collection: MongoCollection[BsonDocument] = database.getCollection("mycollection", classOf[BsonDocument])
+
+        val user = User(UUID.randomUUID(), "Bob", "Boo", "bob@gmail.com")
+
+        collection.insertOne(user.asDocument).subscribe(NoOpsSubscriber[InsertOneResult])
+      }
+    }
+}
